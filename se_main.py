dbbddbe0ea1e9e1504b32ab1d26146338ccce29f
unknown <brstar96@naver.com>
2019-11-30 04:31:19 +0900

    1130 add SE-ResNet and shufflenet

--- se_main.py
+++ se_main.py
@@ -1,8 +1,4 @@
-import os
-import argparse
-import sys
-import time
-import cv2
+import cv2, math, time, sys, argparse, os
 import numpy as np
 import nsml
 from nsml.constants import DATASET_PATH, GPU_NUM
@@ -12,10 +8,16 @@ import torch.nn as nn
 from utils.AdamW import AdamW
 from utils.RAdam import RAdam
 from torch.optim.lr_scheduler import LambdaLR
+from backbones.SENet import senet154, se_resnet101, se_resnet152, se_resnext50_32x4d, se_resnext101_32x4d
+from backbones.shufflenet import shufflenet_v2_x2_0
 from utils import lr_scheduler
-import evaluation.data_loader as testdataloader
 import torchvision
 import torchvision.transforms as transforms
+
+import math
+
+import torch
+import torch.nn as nn
 
 def bind_model(model):
     def save(dir_name):
@@ -111,6 +113,7 @@ class WarmupCosineWithHardRestartsSchedule(LambdaLR):
         if progress >= 1.0:
             return 0.0
         return max(0.0, 0.5 * (1. + math.cos(math.pi * ((float(self.cycles) * progress) % 1.0))))
+
 def preprocessing(data): 
     print('Preprocessing start')
     # 자유롭게 작성해주시면 됩니다.
@@ -124,161 +127,6 @@ def preprocessing(data):
     print('The shape of X changed',X.shape)
     
     return X
-class depthwise_separable_conv(nn.Module):
-    def __init__(self, nin, nout, kernel_size, padding, bias=False):
-        super(depthwise_separable_conv, self).__init__()
-        self.depthwise = nn.Conv2d(nin, nin, kernel_size=kernel_size, padding=padding, groups=nin, bias=bias)
-        self.pointwise = nn.Conv2d(nin, nout, kernel_size=1, bias=bias)
-
-    def forward(self, x):
-        out = self.depthwise(x)
-        out = self.pointwise(out)
-        return out
-
-class Xception(nn.Module):
-    def __init__(self, input_channel, num_classes=3):
-        super(Xception, self).__init__()
-
-        # Entry Flow
-        self.entry_flow_1 = nn.Sequential(
-            nn.Conv2d(input_channel, 32, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(32),
-            nn.ReLU(True),
-
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True)
-        )
-
-        self.entry_flow_2 = nn.Sequential(
-            depthwise_separable_conv(64, 128, 3, 1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-
-            depthwise_separable_conv(128, 128, 3, 1),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        )
-
-        self.entry_flow_2_residual = nn.Conv2d(64, 128, kernel_size=1, stride=2, padding=0)
-
-        self.entry_flow_3 = nn.Sequential(
-            nn.ReLU(True),
-            depthwise_separable_conv(128, 256, 3, 1),
-            nn.BatchNorm2d(256),
-
-            nn.ReLU(True),
-            depthwise_separable_conv(256, 256, 3, 1),
-            nn.BatchNorm2d(256),
-
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        )
-
-        self.entry_flow_3_residual = nn.Conv2d(128, 256, kernel_size=1, stride=2, padding=0)
-
-        self.entry_flow_4 = nn.Sequential(
-            nn.ReLU(True),
-            depthwise_separable_conv(256, 728, 3, 1),
-            nn.BatchNorm2d(728),
-
-            nn.ReLU(True),
-            depthwise_separable_conv(728, 728, 3, 1),
-            nn.BatchNorm2d(728),
-
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        )
-
-        self.entry_flow_4_residual = nn.Conv2d(256, 728, kernel_size=1, stride=2, padding=0)
-
-        # Middle Flow
-        self.middle_flow = nn.Sequential(
-            nn.ReLU(True),
-            depthwise_separable_conv(728, 728, 3, 1),
-            nn.BatchNorm2d(728),
-
-            nn.ReLU(True),
-            depthwise_separable_conv(728, 728, 3, 1),
-            nn.BatchNorm2d(728),
-
-            nn.ReLU(True),
-            depthwise_separable_conv(728, 728, 3, 1),
-            nn.BatchNorm2d(728)
-        )
-
-        # Exit Flow
-        self.exit_flow_1 = nn.Sequential(
-            nn.ReLU(True),
-            depthwise_separable_conv(728, 728, 3, 1),
-            nn.BatchNorm2d(728),
-
-            nn.ReLU(True),
-            depthwise_separable_conv(728, 1024, 3, 1),
-            nn.BatchNorm2d(1024),
-
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        )
-        self.exit_flow_1_residual = nn.Conv2d(728, 1024, kernel_size=1, stride=2, padding=0)
-        self.exit_flow_2 = nn.Sequential(
-            depthwise_separable_conv(1024, 1536, 3, 1),
-            nn.BatchNorm2d(1536),
-            nn.ReLU(True),
-
-            depthwise_separable_conv(1536, 2048, 3, 1),
-            nn.BatchNorm2d(2048),
-            nn.ReLU(True)
-        )
-
-        self.linear = nn.Linear(2048, num_classes)
-
-    def forward(self, x):
-        entry_out1 = self.entry_flow_1(x)
-        entry_out2 = self.entry_flow_2(entry_out1) + self.entry_flow_2_residual(entry_out1)
-        entry_out3 = self.entry_flow_3(entry_out2) + self.entry_flow_3_residual(entry_out2)
-        entry_out = self.entry_flow_4(entry_out3) + self.entry_flow_4_residual(entry_out3)
-
-        middle_out = self.middle_flow(entry_out) + entry_out
-
-        exit_out1 = self.exit_flow_1(middle_out) + self.exit_flow_1_residual(middle_out)
-        exit_out2 = self.exit_flow_2(exit_out1)
-
-        exit_avg_pool = F.adaptive_avg_pool2d(exit_out2, (1, 1))
-        exit_avg_pool_flat = exit_avg_pool.view(exit_avg_pool.size(0), -1)
-
-        output = self.linear(exit_avg_pool_flat)
-
-        return output
-
-# class ConvNet(nn.Module):
-#     def __init__(self, num_classes=3):
-#         super(ConvNet, self).__init__()
-#         self.layer1 = nn.Sequential(
-#             nn.Conv2d(1, 4, kernel_size=5, stride=3, padding=2),
-#             nn.BatchNorm2d(4),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2, stride=2))
-#         self.layer2 = nn.Sequential(
-#             nn.Conv2d(4, 8, kernel_size=5, stride=2, padding=2),
-#             nn.BatchNorm2d(8),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2, stride=2))
-#         self.layer3 = nn.Sequential(
-#             nn.Conv2d(8, 16, kernel_size=5, stride=1, padding=2),
-#             nn.BatchNorm2d(16),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2, stride=2))
-#         self.fc = nn.Linear(3808, num_classes)
-#
-#     def forward(self, x):
-#         out = self.layer1(x)
-#         #print(out.shape)
-#         out = self.layer2(out)
-#         #print(out.shape)
-#         out = self.layer3(out)
-#         #print(out.shape)
-#         out = out.reshape(out.size(0), -1)
-#         #print(out.shape)
-#         out = self.fc(out)
-#         return out
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
@@ -302,35 +150,22 @@ if __name__ == '__main__':
                                  'WarmupCosineWithHardRestartsSchedule'],
                         help='Set lr scheduler mode: (default: WarmupCosineSchedule)')
     config = args.parse_args()
+
     # training parameters
     num_epochs = config.epoch
     batch_size = config.batch_size
     num_classes = config.num_classes
     learning_rate = config.learning_rate
     optimizer = config.optimizer
-    validation_ratio = 0.1 #validation
     lr_scheduler = config.lr_scheduler
 
     random_seed = 2019
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    model = Xception(1, 3)
+    model = shufflenet_v2_x2_0(pretrained=False)
     model.to(device)
-    # model = ConvNet(num_classes).to(device)
+
     # Loss and optimizer
     criterion = nn.CrossEntropyLoss()
-    # # Define Optimizer
-    # if args.optimizer.lower() == 'sgd':
-    #     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
-    # elif args.optimizer.lower() == 'adamw':
-    #     optimizer = AdamW(model.parameters(), lr=args.lr, betas=(args.beta1, args.beta2), weight_decay=args.weight_decay)
-    # elif args.optimizer.lower() == 'radam':
-    #     optimizer = RAdam(model.parameters(), lr=args.lr, betas=(args.beta1, args.beta2), weight_decay=args.weight_decay)
-    # elif args.optimizer.lower() == 'adam':
-    #     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    # else:
-    #     print("Wrong optimizer args input.")
-    #     raise NotImplementedError
-
     optimizer = RAdam(model.parameters(), lr=learning_rate, betas=(0.9, 0.999), weight_decay=1e-4)
 
     bind_model(model)
@@ -341,8 +176,8 @@ if __name__ == '__main__':
     if config.mode == 'train': ### training mode 일때는 여기만 접근
         print('Training Start...')
         # train mode 일때, path 설정
-        nsml.load(checkpoint='1', session='team059/KHD2019_MAMMO/48')           # load시 수정 필수!
-        nsml.save(100)
+        # nsml.load(checkpoint='1', session='team059/KHD2019_MAMMO/48')           # load시 수정 필수!
+        # nsml.save(100)
         # print('model_tmp_save')
         img_path = DATASET_PATH + '/train/'
         data, y = data_loader(img_path)
@@ -354,7 +189,7 @@ if __name__ == '__main__':
                                     shuffle=True)
 
         # Define learning rate scheduler
-        warmup_steps = int(len(batch_loader) * 0.15)
+        warmup_steps = int(len(batch_loader) * 0.1)
         lr_scheduler = WarmupCosineWithHardRestartsSchedule(optimizer=optimizer, warmup_steps=warmup_steps,
                                                          t_total=len(batch_loader) , cycles=1.0, last_epoch=-1)
         # Train the model
@@ -372,8 +207,7 @@ if __name__ == '__main__':
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-                #--------------- 원본
+                lr_scheduler.step()
 
-                
-            nsml.report(summary=True, step=epoch, epoch_total=num_epochs, loss=loss.item(),lr=get_lr(optimizer))#, acc=train_acc)
-            nsml.save(epoch)
+                nsml.report(summary=True, step=epoch, epoch_total=num_epochs, loss=loss.item())#, acc=train_acc)
+                nsml.save(epoch)
